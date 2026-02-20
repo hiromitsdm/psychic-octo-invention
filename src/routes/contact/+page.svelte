de16fbf89238526341234ec6ffd7b24a613d2371
Hiroki <hoga@mit.edu>
2026-02-19 21:36:01 -0500

    Update +page.svelte

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,3 +1,28 @@
 <svelte:head>
   <title>Contact me</title>
+      <nav>
+    <a href="../">Home</a>
+    <a href="../projects/">Projects</a>
+    <a href="../cv/">CV</a>
+    <a href="../contact/" class="current">Contact</a>
+    <a href="https://github.com/hiromitsdm" target="_blank">GitHub</a>
+    </nav>
+    <h1>Contact</h1>
+        
+    <form action="mailto:hoga@mit.edu" method="GET">
+    <label>
+        Email:
+        <input name="email" type="email" value="test@example.com">
+    </label>
+    <label>
+        Subject:
+        <input name="subject" value="Test Subject">
+    </label>
+    <label>
+        Message:
+        <textarea name="body">This is a test message.</textarea>
+    </label>
+    <button>Submit</button>
+    </form>
+    <link rel="stylesheet" href="../style.css">
 </svelte:head>
