7d82ada55b44702f1929c62bb9fbb8a90bd2c234
Hiroki <hoga@mit.edu>
2026-02-26 20:44:38 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,1 +1,9 @@
-YOLO <slot />
+<nav>
+    <a href="." class="current">About</a>
+    <a href="projects">Projects</a>
+    <a href="resume">Resume</a>
+    <a href="contact">Contact</a>
+    <a href="https://github.com/hiromitsdm" target="_blank">GitHub</a> 
+</nav>
+
+<slot />
