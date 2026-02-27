8474bd27361923412043cf95116795a8af0a383d
Hiroki <hoga@mit.edu>
2026-02-26 21:13:32 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -14,9 +14,13 @@ let pages = [
 <nav>
   {#each pages as p}
     <a href={base + p.url}
-      class:current={true}
-      target={p.url.startsWith("http") ? "_blank" : null}
-    >{p.title}</a>
+        class:current={p.url === "/" // is this link the home page?
+        ? $page.url.pathname === (base + "/") // if yes - set current = true if the path name matches. Else, set current = true if the path name starts correctly
+        : $page.url.pathname.startsWith(base + p.url)}
+        target={p.url.startsWith("http") ? "_blank": null}
+>
+  {p.title}
+</a>
   {/each}
 </nav>
 
