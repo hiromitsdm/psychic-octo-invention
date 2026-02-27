22c36dce9288326374944704ededd71144f3a1bd
Hiroki <hoga@mit.edu>
2026-02-26 22:55:24 -0500

    Update +page.svelte

--- src/routes/contact/+page.svelte
+++ src/routes/contact/+page.svelte
@@ -1,14 +1,25 @@
+<script>
+function handleSubmit(event) {
+    event.preventDefault();
+    let form = event.target;
+    let data = new FormData(form);
+
+    let url = form.action + "?";
+    for (let [name, value] of data) {
+        url += (name + "=" + encodeURIComponent(value) + "&");
+    }
+
+    location.href = url;
+}
+</script>
+
 <svelte:head>
   <title>Contact me</title>
 </svelte:head>
 
-    <h1>Contact</h1>
-        
-    <form action="mailto:hoga@mit.edu" method="GET">
-    <label>
-        Email:
-        <input name="email" type="email" value="test@example.com">
-    </label>
+<h1>Contact</h1>
+
+<form action="mailto:hoga@mit.edu" method="GET" on:submit={handleSubmit}>
     <label>
         Subject:
         <input name="subject" value="Test Subject">
@@ -18,5 +29,4 @@
         <textarea name="body">This is a test message.</textarea>
     </label>
     <button>Submit</button>
-    </form>
-    <link rel="stylesheet" href="../style.css">
+</form>
