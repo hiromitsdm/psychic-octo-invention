89b654f14e3dc61ae3f22120c3d1d7c7157a4ff1
Hiroki <hoga@mit.edu>
2026-02-26 20:59:00 -0500

    Update +layout.svelte

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,9 +1,19 @@
+<script>
+import { base } from "$app/paths";
+
+let pages = [
+  {url: "/", title: "About"},
+  {url: "/projects", title: "Projects"},
+  {url: "/resume", title: "Resume"},
+  {url: "/contact", title: "Contact"},
+  {url: "https://github.com/hiromitsdm", title: "Github"},
+];
+</script>
+
 <nav>
-    <a href="." class="current">About</a>
-    <a href="projects">Projects</a>
-    <a href="resume">Resume</a>
-    <a href="contact">Contact</a>
-    <a href="https://github.com/hiromitsdm" target="_blank">GitHub</a> 
+  {#each pages as p}
+    <a href={base + p.url}>{p.title}</a>
+  {/each}
 </nav>
 
 <slot />
