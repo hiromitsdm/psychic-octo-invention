4c8c120dccc9e6ae88ad2859e959690926d92bed
Hiroki <hoga@mit.edu>
2026-03-13 09:36:01 -0400

    Update Bar.svelte

--- src/lib/Bar.svelte
+++ src/lib/Bar.svelte
@@ -1,1 +1,17 @@
-Hello from Bar.svelte
+<script>
+  import * as d3 from 'd3';
+
+  let width = 400;
+  let height = 300;
+</script>
+
+<svg viewBox="0 0 {width} {height}">
+</svg>
+
+<style>
+  svg {
+    max-width: 100%;
+    height: auto;
+    overflow: visible;
+  }
+</style>
