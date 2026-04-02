6ca11ce03b67b114bd78f9e164f1df5bb69679aa
Hiroki <hoga@mit.edu>
2026-04-02 14:01:01 -0400

    Update +page.svelte

--- src/routes/meta/+page.svelte
+++ src/routes/meta/+page.svelte
@@ -19,7 +19,19 @@
     }));
     barData = d3.rollups(locData, v => v.length, d => d.type)
       .map(([lang, count]) => ({ label: lang, value: count }));
-    commits = d3.groups(locData, d => d.commit);
+    commits = d3.groups(locData, d => d.commit).map(([commit, lines]) => {
+      let first = lines[0];
+      let {author, date, time, timezone, datetime} = first;
+      let ret = {
+        id: commit,
+        url: "https://github.com/hiromitsdm/psychic-octo-invention/commit/" + commit,
+        author, date, time, timezone, datetime,
+        hourFrac: datetime.getHours() + datetime.getMinutes() / 60,
+        totalLines: lines.length,
+        lines: lines
+      };
+      return ret;
+    });
     console.log(commits);
   });
 </script>
